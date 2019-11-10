2448b4bbc5a494832694e39e5668e831ec83e6c5
Jordan Hayes <nosafoundation@gmail.com>
2019-11-10 01:58:43 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,10 @@
 def square_array(numbers)
   numbers.each do |number|
-    puts 
+    puts numbers ** 
   end 
 end
 def square_array(new_numbers)
   new_numbers.each do |numbers|
-    
+    puts new_numbers **
   end 
 end
